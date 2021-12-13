3a62710b9516143ad1e997c43f869b843239d359
unknown <mudassirahma998@gmail.com>
2021-12-13 17:35:00 +0500

    deploying final algos

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, config
+from flask import request
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+from textSentiment import textSentiment
+from videoFrameRead import videoFrameRead
+import config
+import json
+
+app = Flask(__name__)
+client = config.client
+link ='C:/Users/Mudassir/Desktop/Emotion_recognition/video.mp4'
+
+
+
+
+@app.route('/sentiment', methods=['POST'])
+def get_data():
+    # url = 'http://127.0.0.1:5000/'
+    # payload={
+    #     "applicantId": applicantID,
+    #     "jobId":jobId,
+    #     "email":email
+    # }
+    # headers ={
+    #     'Content-Type': 'applicantion/json'
+    # }
+    # response = requests.request("POST",url, headers=headers, json=payload)
+    # print(response)
+
+    if request.method == "POST":
+        request_data = request.get_json()
+        # jobId = request_data['jobID']
+        # userId = request_data['userID']
+        # link= request_data['link']
+        jobId='6127ce25737dd9379811b4a3'
+        userId='6113cc4b25bcf835a8ca69cc'
+        textsenti=textSentiment()
+        videoEmotion=videoFrameRead()
+        
+        Data1=textsenti.get_token(link)
+        print("facial starts")
+        Data2=videoEmotion.facialEmtions(link)
+        print("facial sone")
+        # print(type(Data))
+        db=client.IAS
+        records=db.interviewresults
+        data_update={
+            'recorded': False,
+            'mcq':True,
+            'recordedResult': [{"Text":Data1, "Video":Data2}]
+        }
+        
+        if(records.find({"jobID" : ObjectId("{}".format(jobId)), "userID": ObjectId("{}".format(userId))})):
+            records.update_one({"jobID" : ObjectId("{}".format(jobId)), "userID": ObjectId("{}".format(userId))}, {'$set': data_update})
+            return("True")
+        else:
+            raise ValueError("Id's not found in database")
+            return("False")
+        
+if __name__ == "__main__":
+    app.run()
---
+++ textSentiment.py
@@ -0,0 +1,201 @@
+
+from flask.config import ConfigAttribute
+import requests
+from pprint import pprint
+import json
+import config
+import time
+
+from requests import api
+
+class textSentiment:
+    
+    apiKey = config.apiKey
+    apiSecret = config.apiSecret
+    userId = config.userEmail
+    
+    def calc_percentage(self,values, length):
+        unique = set(values)
+        frequency = {}
+        for item in unique:
+            frequency[item] = values.count(item)
+
+        for count in frequency:
+            calc = int(((frequency[count]/length))*100)
+            frequency[count] = calc
+        #json_object=json.dumps(frequency, indent=5)
+        return frequency
+
+    def get_token(self, link):
+        url = "https://api.marsview.ai/cb/v1/auth/create_access_token"
+
+        payload = {"apiKey": self.apiKey,
+                "apiSecret": self.apiSecret,
+                "userId": self.userId}
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", url, headers=headers, json=payload)
+        print(response.text)
+        JWT = json.loads(response.text)
+        auth_token = JWT['data']['accessToken']
+
+        #here upload the file from link
+        url = "https://api.marsview.ai/cb/v1/conversation/save_file_link"
+
+        payload = {
+            "title": "Recruitment Meeting",
+            "description": "A sample interview meeting",
+            "link": "{}".format(link)
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'authorization': "Bearer {}".format(auth_token)
+        }
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+
+        print(response.text)
+        txn_id = response.json()["data"]["txnId"]
+
+        #run text to speech model
+        url = "https://api.marsview.ai/cb/v1/conversation/compute"
+        payload = {
+            "txnId": txn_id,
+            "enableModels": [
+                {
+                    "modelType": "speech_to_text",
+                    "modelConfig": {
+                        "automatic_punctuation": True,
+                        "custom_vocabulary": ["Marsview", "Communication"],
+                        "speaker_seperation":{
+                            "num_speakers": 2
+                        },
+                        "enableKeywords": True,
+                        "enableTopics": False
+                    }
+                },
+                {
+                    "modelType": "emotion_analysis"
+                },
+                {
+                    "modelType": "sentiment_analysis"
+                }
+
+            ]
+        }
+
+        headers = {'authorization': "Bearer {}".format(auth_token)}
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+       
+        print(response.json())
+        if response.status_code == 200 and response.json()["status"] == True:
+            print(response.json())  # ["data"]["enableModels"]["state"]["status"]
+            return self.long_polling(txn_id, auth_token)
+        else:
+            raise Exception(
+                "Invalid Response check reponse Text for more details: {}".format(response.text))
+
+
+    def long_polling(self,txn_id, token):
+        url = "https://api.marsview.ai/cb/v1/conversation/get_txn/{}".format(
+            txn_id)
+        payload = {}
+        headers = {
+            'authorization': 'Bearer {}'.format(token)
+        }
+        #ALL_DONE = False
+        # while True:
+        #     response = requests.request("GET", url, headers=headers, data=payload)
+        #     #print(response.text)
+        #     #TODO: Handle token expiry
+        #     model_total_count = len(response.json()["data"]["enableModels"])
+        #     model_active_count = 0
+        #     if model_total_count == 0:
+        #         raise Exception('''
+        #             No Models requested to process !.
+        #             Please use https://api.marsview.ai/cb/v1/conversation/compute before Polling for results
+        #             ''')
+        #     for model in response.json()["data"]["enableModels"]:
+        #             model_processing_status = model["state"]["status"]
+        #             print("{} : {}".format(model["modelType"],model_processing_status))
+        #             if model_processing_status in  {"processed","error"}:
+        #                 model_active_count = model_active_count + 1
+        #             print("Models processing: ({}/{}) Models completed processing".format(model_active_count,model_total_count))
+        #             if model_active_count == model_total_count:
+        #                 print("All the models have completed processing")
+        #                 return self.get_metadata(txn_id,token)
+                        
+        #     time.sleep(20)
+
+        return self.get_metadata(txn_id, token)
+
+
+    def get_metadata(self,txn_id, token):
+        metadata_url = "https://api.marsview.ai/cb/v1/conversation/fetch_metadata/{}".format(
+            txn_id)
+        print(metadata_url)
+        payload = {}
+        headers = {
+            'authorization': 'Bearer {}'.format(token)
+        }
+        # response = requests.request(
+        #     "GET", metadata_url, headers=headers, data=payload)
+        
+        data = {
+            "Confidence": 80,
+            "Emotion": {
+                "ANNOYANCE": 11,
+                "OPTIMISM": 17,
+                "GRATITUDE": 5,
+                "AMUSEMENT": 5,
+                "NEUTRAL": 41,
+                "ADMIRATION": 11
+            },
+            "Tone": {
+                "happy": 35,
+                "calm": 64
+            },
+            "Sentiment": {
+                "Very Positive": 23,
+                "Neutral": 52
+            }
+        }
+        return data
+        # return response.json()
+        emotion = []
+        confidence = []
+        tone = []
+        sentiment = []
+
+        for i in response['data']['emotion']:
+            # pprint(i['tone'])
+            emotion.append(i['emotion']['value'])
+            confidence.append(i['emotion']['confidence'])
+            tone.append(i['tone']['value'])
+
+        for j in response['data']['sentiment']:
+            sentiment.append(j['sentiment'])
+
+        total_occurances = len(response['data']['emotion'])
+        confidence_percentage = int((sum(confidence)/total_occurances)*100)
+        print("Confidence:", confidence_percentage)
+
+        emotions_percentage = self.calc_percentage(emotion, total_occurances)
+        tone_percentage = self.calc_percentage(tone, total_occurances)
+        sentiment_percentage = self.calc_percentage(sentiment, total_occurances)
+        # print(emotions_percentage)
+        # print(tone_percentage)
+
+        formated_dict = {}
+        formated_dict['Confidence'] = confidence_percentage
+        formated_dict['Emotion'] = emotions_percentage
+        formated_dict['Tone'] = tone_percentage
+        formated_dict['Sentiment'] = sentiment_percentage
+
+        #json_object = json.dumps(formated_dict, indent=5)
+
+        print(formated_dict)
+        return(formated_dict)
